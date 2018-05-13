bfbda6e4a0cca30f700e46d4a8733788d145bb17
user <michael@terminallabs.com>
2018-05-12 21:49:29 -0500

    addded in some simple cuda examples

---
+++ with_cuda.py
@@ -0,0 +1,23 @@
+import numpy as np
+from timeit import default_timer as timer
+from numba import vectorize
+
+@vectorize(['float32(float32, float32)'], target='cuda')
+def pow(a, b):
+    return a ** b
+
+def main():
+    vec_size = 10000000
+
+    np.random.seed(1)
+    a = b = np.array(np.random.sample(vec_size), dtype=np.float32)
+    c = np.zeros(vec_size, dtype=np.float32)
+
+    start = timer()
+    c = pow(a, b)
+    duration = timer() - start
+    print(c)
+    print(duration)
+
+if __name__ == '__main__':
+    main()
